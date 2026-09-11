f0cb971314b31ce15e3eb536b0daffc8ccb9ae51
ron <abilfattoeyo@gmail.com>
2026-09-11 18:03:49 +0900

    feat: learn strings in python

--- primitive.py
+++ primitive.py
@@ -10,3 +10,26 @@ print(f"the count: {count}, and type: {count_type}")  # f = superString
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state
 print(result1, result2)
+
+
+print("====== string ======")
+# Usefull Methods: upper(); lower(); title(); find(); replace();
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"result (1):", {result})
+
+result = course.title()
+print(f"result (2):", {result})
+
+result = course.upper()
+print(f"result (3):", {result})
+
+result = course.replace("FullStack", "MasterClass")
+print(f"result (4):", {result})
+# baribir boshlangich qiymati "AI Python FullStack", sabab "reference" boshqa
+print(course)
+
+# Agar "reference"ni o'zgartirmoqchi bo'lsak:
+course = course.replace("FullStack", "MasterClass")
+print(f"result (another reference):", {course})
