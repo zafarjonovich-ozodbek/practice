8eef3e96b05475a18b8ed1f06ffd3b02b61a52b3
ron <abilfattoeyo@gmail.com>
2026-09-11 18:27:24 +0900

    feat: learn boolen in python

--- primitive.py
+++ primitive.py
@@ -1,7 +1,7 @@
 # in JAVA & C, variable is a name storage location (ma'lumot manzilining nomlanishi)
 # in PYTHON, variable is named reference (manzilning nomlanishi)
 
-print("====== number =====")  # or shorthand > print("="*10)
+print("=========== number ===========")  # or shorthand > print("="*10)
 
 count = 100
 count_type = type(count)
@@ -12,24 +12,44 @@ result2 = count.numerator  # state
 print(result1, result2)
 
 
-print("====== string ======")
+print("============ string =============")
 # Usefull Methods: upper(); lower(); title(); find(); replace();
 
 course = "AI Python FullStack"
 result = type(course)
 print(f"result (1):", {result})
 
-result = course.title()
+result = course.title()  # method
 print(f"result (2):", {result})
 
-result = course.upper()
+result = course.upper()  # method
 print(f"result (3):", {result})
 
-result = course.replace("FullStack", "MasterClass")
+result = course.replace("FullStack", "MasterClass")  # method
 print(f"result (4):", {result})
 # baribir boshlangich qiymati "AI Python FullStack", sabab "reference" boshqa
 print(course)
 
 # Agar "reference"ni o'zgartirmoqchi bo'lsak:
-course = course.replace("FullStack", "MasterClass")
+course = course.replace("FullStack", "MasterClass")  # method
 print(f"result (another reference):", {course})
+
+
+print("============ boolean =============")
+# FUNCTIONS > input(); type(); bool(); int(); str();
+
+y = input("Give the value for y: ")
+print("y:", y)
+
+result = y.isnumeric()  # method "isnumeric" - bu "raqammi?" degan method
+print(f"the input value is numeric: {result}")
+
+
+# TRUTHY: True, 100, -100, "mit"
+# FALSY: False, 0, None, ""
+
+falsy_test = "" or 0 or False or None
+print("falsy_test:", bool(falsy_test))
+
+truthy_test = 'mit'
+print("truthy_test:", bool(truthy_test))
