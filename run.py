cf57113207d40637acce794e410a43843a6fe445
ron <abilfattoeyo@gmail.com>
2026-09-11 17:30:56 +0900

    feat: python system variables & special  methods via dunders

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
-a = 140
-print("a", a)
-message = "hello world"
+# Dunder(dubble underscore) --> dunder variable ("__builtins__"), dunder methos ("__init__")
+# "__builtins__ dunder" - bu "PYTHON"ning sistem variablesi xisoblanadi
+# "__init__ dunder" - bu maxsus method bo'lib, "PYTHON"ning negizi xisoblanadi
+
+# PYTHONning ishlashi "Dunder"ning maxsus
+# 'variable' va 'method'lariga asoslanadi
+
+message = "in PYTHON: everything is OBJECT"
 print(message)
+
+result = type(message)
+print(result)
+
+
+'''Python BUILTIN tools:
+-1) TYPES > int; float; str; list; dict;
+-2) FUNCTIONS > print(); len(); input(); type();
+-3) CONSTANTS > True; False; None;
+'''
+
+# "Dunder"larni ko'rish uchun:
+print(dir(__builtins__))
