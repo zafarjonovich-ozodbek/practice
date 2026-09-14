6addc8059ca94ba0a049ceaca570f6c571168047
ron <abilfattoeyo@gmail.com>
2026-09-14 18:01:28 +0900

    feat: python special/magic methods

--- class.about.py
+++ class.about.py
@@ -47,3 +47,48 @@ print(new_message)
 
 # static method
 Person.explain()
+
+
+print("========== Special/Magic methods ===========")
+# "Python"ning eng kop ishlatiladigan "special/magic" methodlairi:
+# __init__, __new__, __str__, __call__, __getitem__, __eq__, __len__
+
+
+class Car():
+    # state
+    description = "This class makes cars"
+
+    # constructor
+    def __new__(cls, *args):
+        print("* __new__ *")
+        return super().__new__(cls)
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # method
+    def start_engine(self):
+        print(f"the {self.name} started engine!")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped engine!")
+
+    def __str__(self):
+        return f"car.name: {self.name} was produced in {self.age} year!"
+
+    def __call__(self):
+        print("Object called as function")
+        return True
+
+
+my_car = Car("Ferrari", 2026)
+my_car.start_engine()
+my_car.stop_engine()
+
+
+print("==========")
+your_car = Car("BMW", 2026)
+print(your_car)
+response = your_car()
+print(response)
