4ad320cb0b800f2a36b967710224e2ec10a2a569
ron <abilfattoeyo@gmail.com>
2026-09-14 15:00:09 +0900

    feat: specific error hangling systems

--- objects.py
+++ objects.py
@@ -29,3 +29,20 @@ print(result1)
 
 result2 = ceil(99.2)
 print(result2)
+
+
+print("============ Error handling system  ==============")
+car = dict(name="Tayota", year=2026, electric=True)
+
+# Agar bizga "Error" yoki "None" emas, xato payiti o'zimiz xoxlagan xato qiymat chiqishi uchun "Try & Except" blogi kerak bo'ladi
+try:
+    print("passed here")  # shunchaki shuyerdan o'tkanligi
+
+    result = car["speed"]
+    print("result:", result)
+except KeyError as err:  # xato payiti qanday javob kelishi
+    print("No speed state property found:", err)
+else:  # to'g'ri payiti
+    print("Executed successfully without errors")
+finally:  # xato/togri bolsaham oxirida chiqadi, buni yozmasakham bo'ladi
+    print("Final closing logic")
